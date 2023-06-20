b7da77ace7d30fdba45142e2adcaaa5df7e8bb03
dwang <xoxo1987@live.cn>
2023-06-20 11:38:29 +0800

    Add bing search api test.

---
+++ azure_cognitiveservice/bing_test.py
@@ -0,0 +1,7 @@
+import requests
+
+headers = {"Ocp-Apim-Subscription-Key": subscription_key}
+params = {"q": search_term, "textDecorations": True, "textFormat": "HTML"}
+response = requests.get(search_url, headers=headers, params=params)
+response.raise_for_status()
+search_results = response.json()
